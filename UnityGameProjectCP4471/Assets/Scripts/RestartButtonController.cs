5fea9ca6cbaf1fbae28d81621fcdb40fb28d1e94
Moh-F-Rad <mfrad@domain.com>
2021-03-31 23:50:00 -0230

    Last Stable Version (Before exit button)

--- UnityGameProjectCP4471/Assets/Scripts/RestartButtonController.cs
+++ UnityGameProjectCP4471/Assets/Scripts/RestartButtonController.cs
@@ -5,18 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class RestartButtonController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
